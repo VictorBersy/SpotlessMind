96bc08ce432a3dfbd982539842704d3584e143fb
Victor Bersy <bigvivi.fr@gmail.com>
2013-07-06 18:50:08 +0200

    Did a typo in index.php :3

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 require_once __DIR__.'/../app/config/config.php';
-require_once __DIR__.'/../app/controlers/pre_load.php';
+require_once __DIR__.'/../app/controllers/pre_load.php';
 require_once __DIR__.'/../app/config/routes.php';
 
 $app->run();
